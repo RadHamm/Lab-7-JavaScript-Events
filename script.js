08be534977271cdbedd75863e6ef81169ed67f73
RadHamm <stevenphamm@hotmail.com>
2024-11-22 22:04:11 -0500

    Created Mouse Event on HTML and Script.js - added simplistic .css to various elements

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const hoverBtn = document.getElementById('hoverBtn');
+const mouseMessage = document.getElementById('mouseMessage');
 
+hoverBtn.addEventListener('mouseenter', () => {
+    mouseMessage.textContent = 'Congrats you hovered over me successfully!';
+
+});
+
+hoverBtn.addEventListener('mouseleave', () => {
+    mouseMessage.textContent = 'Hover over the button to change this message.';
+
+});
 
