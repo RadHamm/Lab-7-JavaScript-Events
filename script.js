f9d1a525a0184cb370e75c277ca27ad766703b4a
RadHamm <stevenphamm@hotmail.com>
2024-11-22 22:27:46 -0500

    Added Form Event in HTML & Script; added comments in Script to better organize each event for myself., Updated css for input field to be near same size as button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Mouse Event; Hover over button changes message.
+
 const hoverBtn = document.getElementById('hoverBtn');
 const mouseMessage = document.getElementById('mouseMessage');
 
@@ -11,10 +13,20 @@ hoverBtn.addEventListener('mouseleave', () => {
 
 });
 
+// Keyboard Event; Captures keypress in input, outputs last input
 const Keyboard = document.getElementById('Keyboard');
 const keyMessage = document.getElementById('keyMessage');
 
 Keyboard.addEventListener('keydown', (event) => {
     keyMessage.textContent = `Last key pressed: ${event.key}`;
+});
+
+// Form Event; Submits any message successfully, prevents default.
+const simpleForm = document.getElementById('simpleForm');
+const formMessage = document.getElementById('formMessage');
+
+simpleForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formMessage.textContent = 'Form submitted successfully!';
 });
 
